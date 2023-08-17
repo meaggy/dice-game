8c844262c3069df17a3c67a8a2d2a030dafd2969
meaggy <glowackm@mcmaster.ca>
2023-08-17 13:52:40 -0400

    Update README, js

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## Introduction
-
-A simple dice tool to auto-generate and roll dice. 
+A simple tool to simulate a dice roll.
 
 ## Usage
-Input the number of players to auto generate and roll away!
+Input the number of players to represent the number of dice needed. Click submit to generate and roll away!
--- index.js
+++ index.js
@@ -8,13 +8,12 @@ form.addEventListener('submit', function(event){
 
     targetDiv.innerHTML = '';
 
-    console.log("Begin");
-
     const input = document.getElementById('inputField').value;
     const quantity = parseInt(input) +1;
     console.log("REPORTING INPUT", typeof(quantity));
-    
+
     for (let i = 1; i < quantity; i++){
+
 
         let randomNumber = Math.floor(Math.random() * 6) + 1;
 
@@ -22,11 +21,9 @@ form.addEventListener('submit', function(event){
         const newPlayer = document.createElement('p');
         const newDie = document.createElement('img');
         
-       // newDiv.setAttribute('class', 'container')
         newPlayer.textContent = `Player ${i}`;
         newDie.setAttribute('src', `assets/images/dice${randomNumber}.png`);
-        // accesibility
-        newDie.setAttribute('alt', `A die with value of ${randomNumber}`);
+        newDie.setAttribute('alt', `A die with value of ${randomNumber}`);  // accesibility
 
         newDiv.appendChild(newPlayer);  
         newDiv.appendChild(newDie);  
